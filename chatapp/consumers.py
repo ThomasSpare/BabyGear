7e14c79d17093c833c48f047e4b6f896bd963915
Spare <t.spare.jkpg@gmail.com>
2023-06-13 15:19:19 +0000

    created admin chatrooms and chatapp routing.py and consumers.py

---
+++ chatapp/consumers.py
@@ -0,0 +1,65 @@
+# app/consumers.py
+import json
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+
+class TextRoomConsumer(WebsocketConsumer):
+    def connect(self):
+
+        self.room_name = self.scope\['url_route'\]['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def receive(self, text_data):
+        # Receive message from WebSocket
+        text_data_json = json.loads(text_data)
+        text = text_data_json['text']
+        sender = text_data_json['sender']
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': text,
+                'sender': sender
+            }
+        )
+
+    def chat_message(self, event):
+        # Receive message from room group
+        text = event['message']
+        sender = event['sender']
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'text': text,
+            'sender': sender
+        }))
+Now, we can create the routing that will handle the consumer you just created. Create a new file called routing.py and paste the code below, which will orchestrate the consumers:
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+# import app.routing
+from django.urls import re_path
+from app.consumers import TextRoomConsumer
+websocket_urlpatterns = [
+    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
+]
+# the websocket will open at 127.0.0.1:8000/ws/<room_name>
+application = ProtocolTypeRouter({
+    'websocket':
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ,
+})
