8750743ccf42c9c302331de9f30a849a30076bad
ThomasSpare <t.spare.jkpg@gmail.com>
2023-06-19 13:27:17 +0200

    switched to using vs code editor

--- chatrooms/settings.py
+++ chatrooms/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['8000-thomasspare-chatroomsgi-2lvcswinlti.ws-eu99.gitpod.io', '']
+ALLOWED_HOSTS = ['http://localhost:3000/']
 
 # Application definition
 
