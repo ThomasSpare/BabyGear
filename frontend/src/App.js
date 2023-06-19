ac3ecdb99ed8313b4cdb59e5651fd1181cab6a76
Spare <t.spare.jkpg@gmail.com>
2023-06-19 18:54:28 +0000

    created TextArea for tutor in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-
+import { styled } from '@mui/material/styles';
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -8,14 +8,75 @@ import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import Paper from "@material-ui/core/Paper";
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Box from '@mui/material/Box';
+
 
 import { withStyles } from "@material-ui/core/styles";
+import { TextareaAutosize } from "@material-ui/core";
 
 const useStyles = (theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 });
+
+
+  const blue = {
+    100: '#DAECFF',
+    200: '#b6daff',
+    400: '#3399FF',
+    500: '#007FFF',
+    600: '#0072E5',
+    900: '#003A75',
+  };
+
+  const grey = {
+    50: '#f6f8fa',
+    100: '#eaeef2',
+    200: '#d0d7de',
+    300: '#afb8c1',
+    400: '#8c959f',
+    500: '#6e7781',
+    600: '#57606a',
+    700: '#424a53',
+    800: '#32383f',
+    900: '#24292f',
+  };
+
+  const StyledTextarea = styled(TextareaAutosize)(
+    ({ theme }) => `
+    width: 450px;
+    font-family: 'JetBrains Mono', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+    font-weight: 600;
+    line-height: 1.5;
+    margin-top: 50px;
+    margin-left: 50px;
+    padding: 12px;
+    border-radius: 12px;
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  
+    &:hover {
+      border-color: ${blue[400]};
+    }
+  
+    &:focus {
+      border-color: ${blue[400]};
+      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
+    }
+  
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `,
+  );
+
 
 class App extends Component {
   state = {
@@ -65,7 +126,12 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Container component="main" maxWidth="xs">
+      <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 3, sm: 2, md: 3 }}>
+      <Grid xs={6}>
+      <StyledTextarea aria-label="empty textarea" placeholder="Waiting on tutor ... " />
+        </Grid>
+      <Grid xs={3} component="main" maxWidth="xs">
         {this.state.filledForm ? (
           <div style={{ marginTop: 50 }}>
             Room Name: {this.state.room}
@@ -74,12 +140,14 @@ class App extends Component {
             >
               {this.state.messages.map((message) => (
                 <>
+                
                   <Card className={classes.root}>
                     <CardHeader title={message.name} subheader={message.msg} />
                   </Card>
                 </>
               ))}
             </Paper>
+          <Button>Hello</Button>
             <form
               className={classes.form}
               noValidate
@@ -145,8 +213,34 @@ class App extends Component {
             </div>
           </div>
         )}
-      </Container>
+      </Grid>
+      </Grid>
+      </Box>
     );
   }
 }
+
 export default withStyles(useStyles)(App);
+
+export function EmptyTextarea() {
+  const blue = {
+    100: '#DAECFF',
+    200: '#b6daff',
+    400: '#3399FF',
+    500: '#007FFF',
+    600: '#0072E5',
+    900: '#003A75',
+  };
+
+  const grey = {
+    50: '#f6f8fa',
+    100: '#eaeef2',
+    200: '#d0d7de',
+    300: '#afb8c1',
+    400: '#8c959f',
+    500: '#6e7781',
+    600: '#57606a',
+    700: '#424a53',
+    800: '#32383f',
+    900: '#24292f',
+  }};
