4d654d546f0a9cc3cc8546e94e643fb0f3a8d2e9
Spare <t.spare.jkpg@gmail.com>
2023-06-27 10:24:06 +0000

    profile admin now in business

--- profiles/views.py
+++ profiles/views.py
@@ -7,5 +7,4 @@ from .serializers import ProfileSerializer
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
         return Response(profiles)
