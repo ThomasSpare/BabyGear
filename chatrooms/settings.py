e667104654bc67b5ed2310f8e335dc85d3b271fb
Spare <t.spare.jkpg@gmail.com>
2023-06-27 15:24:54 +0000

    secret key in settings edited - said DJANGO SECRET before

--- chatrooms/settings.py
+++ chatrooms/settings.py
@@ -49,7 +49,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
