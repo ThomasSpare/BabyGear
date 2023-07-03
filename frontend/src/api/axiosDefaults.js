0f67099c9345ca5fcb2d07d15684b809b55d3c28
Spare <t.spare.jkpg@gmail.com>
2023-07-03 08:00:41 +0000

    delete frontend from my backend repo

--- frontend/src/api/axiosDefaults.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "Heroku delployed api url";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
