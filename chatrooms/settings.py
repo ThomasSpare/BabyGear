329927c9d63d09f9a48817817b0594069776afcd
Spare <t.spare.jkpg@gmail.com>
2023-10-23 12:10:35 +0000

    Registration now working on frontend outside of web security CORS issues

--- chatrooms/settings.py
+++ chatrooms/settings.py
@@ -57,10 +57,10 @@ CORS_ALLOW_CREDENTIALS = True
 # ]
 
 REST_USE_JWT = True
-JWT_AUTH_SECURE = True
+JWT_AUTH_SECURE = False
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
+JWT_AUTH_SAMESITE = 'Lax'
 
 # Application definition
 
